94516a6943662de192b00b379e27d080bbcf3645
Vikram Belthur <46857503+VikBelt@users.noreply.github.com>
2020-07-02 22:11:08 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+//Using this main.c to check the code in stringtype
+
 #include <stdio.h>
 #include "stringtype.h"
 
@@ -5,6 +7,7 @@ int main(){
     String str;
     createString(&str,"Hello");
     append(&str,"World");
+    //I added a print function
     println(str);
     deleteString(&str);
     return 0;
